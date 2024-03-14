21c39c0fc73526e30fb907669dd72ec0b5dc367f
Roshan Mishra <roshanmishrarinem@gmail.com>
2024-03-14 20:42:48 +0530

    feat: initial oauth setup with the api and updated readme

---
+++ README.md
@@ -0,0 +1,74 @@
+<div align="center">
+  <h1>🚀 AniXplore - A MyAnimeList CLI Client</h1>
+</div>
+
+A command line client built with Go that interfaces MyAnimeList API for viewing top anime, anime details, updating user's anime lists and more
+
+---
+
+# 📌 Prerequisites
+
+- **MyAnimeList Account** is required to obtain `ClientId` and `ClientSecret`.
+
+---
+
+# 🔧 Configuration
+
+To get started, you'll need a 'Client ID' and 'Client Secret' from MyAnimeList's API Dashboard:
+
+1. 🔗 **Navigate to**: [MyAnimeList API Dashboard](https://myanimelist.net/apiconfig)
+
+2. 🚪 **Sign in or create a MyAnimeList account**.
+
+3. ➕ **Click on 'Create  ID'**
+
+4. 📜 **Fill in the app details**.
+
+5. 🌐 **In the App Redirect URL field of the app you created, please enter the following URLs:**
+
+   - 📎 `http://localhost:9999/oauth/callback`
+
+6. 🛍 **Once the App is created**, you'll find the 'Client ID' and 'Client Secret' on the app details page.
+
+7. 🔑 **Add Credentials in .env**:
+    - Create a .env file in the root directory of this project and copy these values by adding your MAL client id and client secret.
+```
+# MAL
+MAL_CLIENT_ID="<CLIENT ID GOES HERE>"
+MAL_CLIENT_SECRET="<CLIENT SECRET GOES HERE>"
+REDIRECT_URL="http://localhost:9999/oauth/callback"
+```
+
+🚫 **Remember**: Keep your 'Client Secret and Client Id' confidential. Never share it! They can be used to control your MyAnimeList data.
+
+---
+
+# 📝 TODO List
+- [x] Setup oauth with MyAnimeList API
+- [ ] Use access token to authenticate User's requests and refresh token when access token expired
+- [ ] Add methods for calling different API endpoints of MAL
+- [ ] Integrate Cobra and add CLI commands to use different methods
+- [ ] Improve UI of the CLI results
+
+---
+
+# 💻 Local Development
+
+1. Install Go version 1.21 or above https://go.dev/
+2. Clone repo
+3. Run `go mod tidy`
+4. Follow `Configuration` steps mentioned above
+5. Now oauth with MyAnimeList can be executed from root directory e.g. `go run main.go`
+
+---
+
+# 🤝 Contributing
+
+Your contributions are welcome! 🌟 Feel free to submit pull requests or raise issues.
+
+---
+
+# 📜 License
+
+This project is under the MIT License. Dive into the `LICENSE` file for more.
+
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/joho/godotenv"
+	"github.com/pkg/browser"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := &oauth2.Config{
+		ClientID:     os.Getenv("MAL_CLIENT_ID"),
+		ClientSecret: os.Getenv("MAL_CLIENT_SECRET"),
+		Scopes:       []string{"read"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:   "https://myanimelist.net/v1/oauth2/authorize",
+			TokenURL:  "https://myanimelist.net/v1/oauth2/token",
+			AuthStyle: oauth2.AuthStyleInParams,
+		},
+		RedirectURL: os.Getenv("REDIRECT_URL"),
+	}
+
+	server := &http.Server{Addr: ":9999"}
+
+	codeChan := make(chan string)
+
+	http.HandleFunc("/oauth/callback", handleOAuthCallback(codeChan))
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	codeVerifier, codeChallenge := generateCodeVerifierAndChallenge()
+
+	url := config.AuthCodeURL("state", oauth2.SetAuthURLParam("code_challenge", codeChallenge))
+
+	fmt.Printf("Your browser has been opened to visit::\n%s\n", url)
+
+	if err := browser.OpenURL(url); err != nil {
+		panic(fmt.Errorf("failed to open browser for authentication %s", err.Error()))
+	}
+
+	code := <-codeChan
+
+	token, err := exchangeAuthorizationCodeForToken(config, code, codeVerifier)
+	if err != nil {
+		log.Fatalf("Failed to exchange authorization code for token: %v", err)
+	}
+
+	if !token.Valid() {
+		log.Fatalf("Cannot get source information without accessToken: %v", err)
+		return
+	}
+
+	if err := writeTokenToFile(token); err != nil {
+		log.Fatalf("Failed to write token to file: %v", err)
+	}
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Failed to shut down server: %v", err)
+	}
+
+	log.Println(color.CyanString("Authentication successful"))
+}
+
+func handleOAuthCallback(codeChan chan string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		queryParts := r.URL.Query()
+
+		code := queryParts.Get("code")
+
+		codeChan <- code
+
+		msg := "<p><strong>Authentication successful</strong>. You may now close this tab.</p>"
+		fmt.Fprint(w, msg)
+	}
+}
+
+func writeTokenToFile(token *oauth2.Token) error {
+	file, err := os.OpenFile("token.json", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return fmt.Errorf("Unable to create token file: %v", err)
+	}
+	defer file.Close()
+
+	if err := json.NewEncoder(file).Encode(token); err != nil {
+		return fmt.Errorf("Unable to write token to file: %v", err)
+	}
+
+	return nil
+}
+
+func generateCodeVerifierAndChallenge() (string, string) {
+	// Generate random bytes
+	randomBytes := make([]byte, 64)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		log.Fatal("Failed to generate random bytes:", err)
+	}
+
+	// Encode the random bytes to base64 URL encoding
+	codeVerifier := base64.URLEncoding.EncodeToString(randomBytes)
+
+	// Trim the padding characters "=" from the end
+	codeVerifier = codeVerifier[:len(codeVerifier)-2]
+
+	return codeVerifier, codeVerifier
+}
+
+func exchangeAuthorizationCodeForToken(config *oauth2.Config, code, codeVerifier string) (*oauth2.Token, error) {
+	values := url.Values{}
+	values.Set("client_id", config.ClientID)
+	values.Set("client_secret", config.ClientSecret)
+	values.Set("code", code)
+	values.Set("redirect_uri", os.Getenv("REDIRECT_URL"))
+	values.Set("code_verifier", codeVerifier)
+	values.Set("grant_type", "authorization_code")
+
+	resp, err := http.PostForm(config.Endpoint.TokenURL, values)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %s", resp.Status)
+	}
+
+	var token oauth2.Token
+	if err := json.NewDecoder(resp.Body).Decode(&token); err != nil {
+		return nil, err
+	}
+
+	return &token, nil
+}
